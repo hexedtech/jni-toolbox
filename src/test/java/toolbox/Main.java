3d3bc8b6d5bfd9a44f9851018d997e526f25e0d2
alemi <me@alemi.dev>
2024-09-24 02:29:31 +0200

    test: vecs, options, nulls, also build with gradle

--- src/test/java/toolbox/Main.java
+++ src/test/java/toolbox/Main.java
@@ -2,6 +2,7 @@ package toolbox;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class Main {
@@ -11,6 +12,8 @@ public class Main {
 
 	static native int sum(int a, int b);
 	static native String concat(String a, String b);
+	static native String[] to_vec(String a, String b, String c);
+	static native boolean maybe(String optional);
 
 	@Test
 	public void argumentsByValue() {
@@ -20,6 +23,29 @@ public class Main {
 	@Test
 	public void argumentsByReference() {
 		assertEquals(Main.concat("hello", "world"), "hello -- world");
+	}
+
+	@Test
+	public void checksForNull() {
+		// TODO maybe these should throw NullPtrException
+		assertThrows(RuntimeException.class, () -> Main.concat("a", null));
+		assertThrows(RuntimeException.class, () -> Main.concat(null, "a"));
+		assertThrows(RuntimeException.class, () -> Main.concat(null, null));
+	}
+
+	@Test
+	public void returnVec() {
+		String[] actual = new String[]{"a", "b", "c"};
+		String[] from_rust = Main.to_vec("a", "b", "c");
+		for (int i = 0; i < 3; i++) {
+			assertEquals(actual[i], from_rust[i]);
+		}
+	}
+
+	@Test
+	public void optional() {
+		assertEquals(Main.maybe(null), false);
+		assertEquals(Main.maybe("aa"), true);
 	}
 	
 }
--- src/test/test.rs
+++ src/test/test.rs
@@ -9,3 +9,13 @@ fn sum(a: i32, b: i32) -> i32 {
 fn concat(a: String, b: String) -> String {
 	format!("{a} -- {b}")
 }
+
+#[jni(package = "toolbox", class = "Main")]
+fn to_vec(a: String, b: String, c: String) -> Vec<String> {
+	vec![a, b, c]
+}
+
+#[jni(package = "toolbox", class = "Main")]
+fn maybe(idk: Option<String>) -> bool {
+	idk.is_some()
+}
